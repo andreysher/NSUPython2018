50382e3b1aea2221215e6c6c429284c13dcffd72
andreysher <30853561+andreysher@users.noreply.github.com>
2018-03-30 18:02:37 +0700

    Update task2.py

--- problems-1/a-shcherbin/task2.py
+++ problems-1/a-shcherbin/task2.py
@@ -1,16 +1,26 @@
-import math
+iimport math
 
 
-def is_prime(number) -> bool:
-    i = 2
-    maximum = math.trunc(math.sqrt(number)) + 1
-    while i < maximum:
-        if number % i == 0:
-            return False
-        i += 1
-    return True
+def to_simple_factors(number) -> list:
+    factors = []
+    current = 2
+    power = 0
+    if number < 2:
+        return factors
+    maximo = math.trunc(math.sqrt(number)) + 1
+    while current < maximo:
+        while number % current == 0:
+            power += 1
+            number = number // current
+        if power > 0:
+            factors.append([current, power])
+        power = 0
+        current += 1
+    if number != 1:
+        factors.append([number, 1])
+    return factors
 
 
 if __name__ == '__main__':
-    simple = is_prime(1488866241517)
-    print(simple)
+    fact = to_simple_factors(12345678874658964)
+    print(fact)
